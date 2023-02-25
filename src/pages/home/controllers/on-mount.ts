1adde1c69cb3a404d03f844d83e2c401adf67785
Jeisson A Valderrama <jeissonvalderramam@gmail.com>
2023-02-25 09:02:21 -0500

    style: opacity
    Option buttons of product small cart. And dollar symbol.

---
+++ src/pages/home/controllers/on-mount.ts
@@ -0,0 +1,4 @@
+
+export function onMount () {
+    document.title = 'Products'
+}
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,10 @@
 import { Header } from './components/header'
 import { SearchProduct } from './components/search-product'
 import { ProductList } from './components/product-list'
+import { onMount } from './controllers/on-mount'
 
 export function Home () {
+    onMount()
 
     return (
         <section className="home page">
